2bb4da47c537b1a947593682fe2adc1ebfe0dfda
svnlogikos <mario.castro@logikos.com.ar>
2020-06-12 19:01:20 -0300

    Desarrollo ProyeccionData y demás

--- src/cine/controlador/ProyeccionData.java
+++ src/cine/controlador/ProyeccionData.java
@@ -1,7 +1,15 @@
 
 package cine.controlador;
 
+import cine.modelo.Proyeccion;
+import cine.modelo.Sala;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ProyeccionData {
@@ -13,7 +21,45 @@ public class ProyeccionData {
         
     }
 
-    public ProyeccionData() {
+    public ProyeccionData() 
+    {
+    }
+    
+    public List<Proyeccion> obtenerProyecciones(Sala sala,LocalDate horario)
+    {
+        List<Proyeccion> proyecciones = new ArrayList<Proyeccion>();
+    
+        try 
+        {
+            String query = "SELECT * FROM proyeccion "
+                            + "WHERE (proyeccion.idSala = ?) AND "
+                            + "((proyeccion.horario_desde <= '?') AND (proyeccion.horario_hasta >= '?'))";
+            PreparedStatement statement = con.prepareStatement(query);
+            statement.setInt(1, sala.getIdSala());
+            statement.setObject(2, horario);
+            statement.setObject(3, horario);
+            
+            ResultSet resultSet = statement.executeQuery();
+            
+            Proyeccion proyeccion;
+            
+            while(resultSet.next())
+            {
+                proyeccion = new Proyeccion();
+                proyeccion.setIdProyeccion(resultSet.getInt("idProyeccion"));
+                proyeccion.setPelicula(resultSet.getInt("idPelicula"));              
+                proyeccion.setSala(resultSet.getInt("idSala"));
+
+                proyecciones.add(proyeccion);
+            }      
+            statement.close();
+        } 
+        catch (SQLException ex) 
+        {
+            System.out.println("ERROR: Obtención de alumnos: " + ex.getMessage());
+        }
+        
+        return proyecciones;
     }
        
 }
--- src/cine/testCine.java
+++ src/cine/testCine.java
@@ -8,6 +8,7 @@ package cine;
 import cine.controlador.*;
 import cine.modelo.Cliente;
 import cine.modelo.Ticket;
+import java.time.LocalDate;
 
 /**
  *
@@ -22,16 +23,22 @@ public class testCine
 
         Cliente cliente = new Cliente(12345679,"Laura","Garcia");
         ClienteData cd = new ClienteData(con);
-        cd.altaCliente(cliente);
+        ProyeccionData pd = new ProyeccionData(con);
+        //cd.altaCliente(cliente);
         //cd.bajaCliente(3);
-        cliente.setNombre("María");
-        cd.modificarCliente(cliente);
+        //cliente.setNombre("María");
+        //cd.modificarCliente(cliente);
 
         /*Cliente prueba = new Cliente(123,"NombrePrueba", "ApellidoPrueba");
         ClienteData cd = new ClienteData(con);
         cd.altaCliente(prueba);
         prueba.setApellido("Pruebasss");
         cd.modificarCliente(prueba);*/
+        
+        System.out.println(cd.buscarcliente(1).getIdCliente());
+        System.out.println(pd.);
+        
+        Ticket ticket = new Ticket(cliente, proyeccion, butaca, LocalDate.MIN, 0, true, metodo_pago);
 
     }
 }
