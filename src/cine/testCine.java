076aeec4b0fde9ddf57c923fcbe99f6024603106
Arezlon <50326537+Arezlon@users.noreply.github.com>
2020-06-13 12:30:45 -0300

    Pruebas en testCine para la entrega del 14/6

--- src/cine/testCine.java
+++ src/cine/testCine.java
@@ -20,25 +20,40 @@ public class testCine
     {
         Conexion con = new Conexion();
         con.getConexion();
-
-        Cliente cliente = new Cliente(12345679,"Laura","Garcia");
         ClienteData cd = new ClienteData(con);
-        ProyeccionData pd = new ProyeccionData(con);
+        
+        // PRUEBAS PARA LA ENTREGA DEL 14/6 - NO BORRAR-
+        // Alta, baja y modificacion de Clientes:
+        
+        // Alta Cliente:
+        Cliente cliente1 = new Cliente(1,"Cliente1","Prueba1");
+        cd.altaCliente(cliente1);
+        
+        // Modificacion Cliente:
+        Cliente cliente2 = new Cliente(2,"Cliente2","Prueba2");
+        cd.altaCliente(cliente2);
+        cliente2.setNombre("Modificado");
+        cd.modificarCliente(cliente2);
+        
+        // Baja Cliente:
+        Cliente cliente3 = new Cliente(3,"Cliente3","Prueba3");
+        cd.altaCliente(cliente3);
+        int id = cliente3.getIdCliente();
+        cd.bajaCliente(id);
+        // FIN PRUEBAS PARA LA ENTREGA DEL 14/6
+        
+        
+        // OTRAS PRUEBAS:
+        //ProyeccionData pd = new ProyeccionData(con);
         //cd.altaCliente(cliente);
         //cd.bajaCliente(3);
         //cliente.setNombre("María");
         //cd.modificarCliente(cliente);
-
-        /*Cliente prueba = new Cliente(123,"NombrePrueba", "ApellidoPrueba");
-        ClienteData cd = new ClienteData(con);
-        cd.altaCliente(prueba);
-        prueba.setApellido("Pruebasss");
-        cd.modificarCliente(prueba);*/
         
-        System.out.println(cd.buscarcliente(1).getIdCliente());
+        /*System.out.println(cd.buscarcliente(1).getIdCliente());
         System.out.println(pd.);
         
-        Ticket ticket = new Ticket(cliente, proyeccion, butaca, LocalDate.MIN, 0, true, metodo_pago);
+        Ticket ticket = new Ticket(cliente, proyeccion, butaca, LocalDate.MIN, 0, true, metodo_pago);*/
 
     }
 }
