d29331bc1fc584d8d1d4b7c3710abc05e8266fb4
SoriaNico <64919459+SoriaNico@users.noreply.github.com>
2020-06-21 20:07:04 -0300

    Modificación de "AltaPeliculasView" y de "BajaModificacionPeliculasView"

--- src/cine/vista/AltaPeliculasView.java
+++ src/cine/vista/AltaPeliculasView.java
@@ -5,17 +5,27 @@
  */
 package cine.vista;
 
+import cine.controlador.PeliculaData;
+import cine.controlador.Conexion;
+import cine.modelo.Pelicula;
+import java.awt.Color;
+import javax.swing.JTextField;
+
 /**
  *
  * @author Arezlon
  */
 public class AltaPeliculasView extends javax.swing.JInternalFrame {
+    private PeliculaData peliculaData;
+    private Conexion con;
 
     /**
      * Creates new form AltaPeliculasView
      */
     public AltaPeliculasView() {
         initComponents();
+        con = new Conexion();
+        peliculaData = new PeliculaData(con);
     }
 
     /**
@@ -27,24 +37,178 @@ public class AltaPeliculasView extends javax.swing.JInternalFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jSeparator1 = new javax.swing.JSeparator();
+        jSeparator2 = new javax.swing.JSeparator();
+        jBVaciar = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLAlerta = new javax.swing.JLabel();
+        jTTituloPelicula = new javax.swing.JTextField();
+        jTIDPelicula = new javax.swing.JTextField();
+        jBGuardar = new javax.swing.JButton();
+
         setClosable(true);
         setTitle("Alta de Peliculas");
+
+        jBVaciar.setText("Vaciar");
+        jBVaciar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBVaciarActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jLabel1.setText("Título*");
+
+        jLabel2.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jLabel2.setText("ID*");
+
+        jLAlerta.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jLAlerta.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLAlerta.setText(" ");
+
+        jTTituloPelicula.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTTituloPeliculaActionPerformed(evt);
+            }
+        });
+        jTTituloPelicula.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                jTTituloPeliculajTValidacionTextosKeyTyped(evt);
+            }
+        });
+
+        jTIDPelicula.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTIDPeliculaActionPerformed(evt);
+            }
+        });
+        jTIDPelicula.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                jTIDPeliculajTValidacionTextosKeyTyped(evt);
+            }
+        });
+
+        jBGuardar.setText("Guardar");
+        jBGuardar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBGuardarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 394, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jSeparator1, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jSeparator2)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(58, 58, 58)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel1))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jTTituloPelicula, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 199, Short.MAX_VALUE)
+                            .addComponent(jTIDPelicula, javax.swing.GroupLayout.Alignment.TRAILING))
+                        .addGap(0, 69, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jLAlerta, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jBGuardar)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jBVaciar)
+                .addGap(125, 125, 125))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 274, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(40, 40, 40)
+                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 47, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jTTituloPelicula, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(jTIDPelicula, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(44, 44, 44)
+                .addComponent(jLAlerta)
+                .addGap(18, 18, 18)
+                .addComponent(jSeparator2, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jBGuardar)
+                    .addComponent(jBVaciar))
+                .addGap(17, 17, 17))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jBVaciarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBVaciarActionPerformed
+        vaciarTodo();
+    }//GEN-LAST:event_jBVaciarActionPerformed
 
+    private void jTTituloPeliculaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTTituloPeliculaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTTituloPeliculaActionPerformed
+
+    private void jTTituloPeliculajTValidacionTextosKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTTituloPeliculajTValidacionTextosKeyTyped
+        char ingreso = evt.getKeyChar();
+        String campo = ((JTextField)evt.getSource()).getText()+ingreso;
+        if(!Character.isAlphabetic(ingreso) || campo.length() > 25){
+            evt.consume();
+        }
+    }//GEN-LAST:event_jTTituloPeliculajTValidacionTextosKeyTyped
+
+    private void jTIDPeliculaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTIDPeliculaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTIDPeliculaActionPerformed
+
+    private void jTIDPeliculajTValidacionTextosKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTIDPeliculajTValidacionTextosKeyTyped
+        char ingreso = evt.getKeyChar();
+        String campo = ((JTextField)evt.getSource()).getText()+ingreso;
+        if(!Character.isAlphabetic(ingreso) || campo.length() > 25){
+            evt.consume();
+        }
+    }//GEN-LAST:event_jTIDPeliculajTValidacionTextosKeyTyped
+
+    private void jBGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBGuardarActionPerformed
+        String titulo = jTTituloPelicula.getText();
+        long idPelicula =  Long.parseLong(jTIDPelicula.getText());
+        Color colorExito = new Color(7, 110, 46);
+        Color colorError = new Color(255, 0, 0);
+        Pelicula pelicula = new Pelicula(titulo);
+        peliculaData.altaPelicula(pelicula);
+        jLAlerta.setText("Creada la película "+pelicula.getTitulo()+" correctamente");
+        jLAlerta.setForeground(colorExito);
+        
+        vaciarTodo();
+    }//GEN-LAST:event_jBGuardarActionPerformed
+
+    private void vaciarTodo(){
+        jTTituloPelicula.setText("");
+        jTIDPelicula.setText("");
+    }
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jBGuardar;
+    private javax.swing.JButton jBVaciar;
+    private javax.swing.JLabel jLAlerta;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JSeparator jSeparator2;
+    private javax.swing.JTextField jTIDPelicula;
+    private javax.swing.JTextField jTTituloPelicula;
     // End of variables declaration//GEN-END:variables
 }
--- src/cine/vista/BajaModificacionPeliculasView.java
+++ src/cine/vista/BajaModificacionPeliculasView.java
@@ -5,17 +5,38 @@
  */
 package cine.vista;
 
+import java.util.ArrayList;
+import cine.controlador.PeliculaData;
+import cine.controlador.Conexion;
+import cine.modelo.Pelicula;
+import java.awt.Color;
+import javax.swing.JTextField;
 /**
  *
  * @author Arezlon
  */
 public class BajaModificacionPeliculasView extends javax.swing.JInternalFrame {
 
+    private PeliculaData peliculaData;
+    private Conexion con;
+    private ArrayList<Pelicula> listaPeliculas;
     /**
      * Creates new form BajaModificacionPeliculasView
      */
     public BajaModificacionPeliculasView() {
         initComponents();
+        con = new Conexion();
+        peliculaData = new PeliculaData(con);
+        
+        cargarDesplegablePeliculas();
+    }
+        
+    private void cargarDesplegablePeliculas(){
+        jCBPeliculas.removeAllItems();
+        listaPeliculas=(ArrayList)peliculaData.obtenerPeliculas();
+        for (Pelicula p:listaPeliculas) {
+            jCBPeliculas.addItem(p);
+        }
     }
 
     /**
@@ -27,24 +48,252 @@ public class BajaModificacionPeliculasView extends javax.swing.JInternalFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLAlerta = new javax.swing.JLabel();
+        jCBPeliculas = new javax.swing.JComboBox<>();
+        jTIDPelicula = new javax.swing.JTextField();
+        jTTituloPelicula = new javax.swing.JTextField();
+        jSeparator1 = new javax.swing.JSeparator();
+        jBBuscar = new javax.swing.JButton();
+        jSeparator2 = new javax.swing.JSeparator();
+        jLabel1 = new javax.swing.JLabel();
+        jBModificar = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jBVaciar = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
+        jBEliminar = new javax.swing.JButton();
+
         setClosable(true);
         setTitle("Baja y modificacion de Peliculas");
+
+        jLAlerta.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jLAlerta.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLAlerta.setText(" ");
+        jLAlerta.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+
+        jCBPeliculas.addItemListener(new java.awt.event.ItemListener() {
+            public void itemStateChanged(java.awt.event.ItemEvent evt) {
+                jCBPeliculasItemStateChanged(evt);
+            }
+        });
+
+        jTIDPelicula.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                jTIDPeliculaKeyTyped(evt);
+            }
+        });
+
+        jTTituloPelicula.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTTituloPeliculaActionPerformed(evt);
+            }
+        });
+        jTTituloPelicula.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                jTTituloPeliculajTValidacionNombresKeyTyped(evt);
+            }
+        });
+
+        jBBuscar.setText("Buscar");
+        jBBuscar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBBuscarActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jLabel1.setText("Películas");
+
+        jBModificar.setText("Modificar");
+        jBModificar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBModificarActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jLabel2.setText("ID");
+
+        jBVaciar.setText("Vaciar");
+        jBVaciar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBVaciarActionPerformed(evt);
+            }
+        });
+
+        jLabel4.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
+        jLabel4.setText("Título");
+
+        jBEliminar.setText("Eliminar");
+        jBEliminar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBEliminarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 394, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jBModificar)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jBVaciar)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jBEliminar)
+                .addGap(87, 87, 87))
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(70, 70, 70)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel4, javax.swing.GroupLayout.Alignment.TRAILING))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jCBPeliculas, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jTTituloPelicula)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jTIDPelicula, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jBBuscar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                        .addContainerGap(77, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jSeparator1)
+                            .addComponent(jSeparator2)
+                            .addComponent(jLAlerta, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addContainerGap())))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 274, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(27, 27, 27)
+                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jCBPeliculas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTIDPelicula, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2)
+                    .addComponent(jBBuscar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTTituloPelicula, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel4))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 50, Short.MAX_VALUE)
+                .addComponent(jLAlerta, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jSeparator2, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jBModificar)
+                    .addComponent(jBVaciar)
+                    .addComponent(jBEliminar))
+                .addGap(19, 19, 19))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jCBPeliculasItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_jCBPeliculasItemStateChanged
+        Pelicula p = (Pelicula)jCBPeliculas.getSelectedItem();
+
+        if(p != null){
+            jTTituloPelicula.setText(p.getTitulo());
+            jTIDPelicula.setText(p.getIdPelicula()+"");
+            jBEliminar.setEnabled(true);         }
+    }//GEN-LAST:event_jCBPeliculasItemStateChanged
+
+    private void jTIDPeliculaKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTIDPeliculaKeyTyped
+        char ingreso = evt.getKeyChar();
+        String campo = ((JTextField)evt.getSource()).getText()+ingreso;
+        if(!Character.isDigit(ingreso) || campo.length() > 8){
+            evt.consume();
+        }
+    }//GEN-LAST:event_jTIDPeliculaKeyTyped
+
+    private void jTTituloPeliculaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTTituloPeliculaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTTituloPeliculaActionPerformed
+
+    private void jTTituloPeliculajTValidacionNombresKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTTituloPeliculajTValidacionNombresKeyTyped
+        char ingreso = evt.getKeyChar();
+        String campo = ((JTextField)evt.getSource()).getText()+ingreso;
+        if(!Character.isAlphabetic(ingreso) || campo.length() > 25){
+            evt.consume();
+        }
+    }//GEN-LAST:event_jTTituloPeliculajTValidacionNombresKeyTyped
+
+    private void jBBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBBuscarActionPerformed
+        int id = Integer.parseInt(jTIDPelicula.getText()) ;
+        Pelicula busquedaPeli = peliculaData.buscarPelicula(id); 
+
+        if(busquedaPeli.getIdPelicula() > 0){
+            jTTituloPelicula.setText(busquedaPeli.getTitulo()+"");
+        }else{
+            Color colorError = new Color(255, 0, 0);
+            jLAlerta.setText("No se encontró una película con el ID ingresado.");
+            jLAlerta.setForeground(colorError);
+            limpiar();
+        }
+
+    }//GEN-LAST:event_jBBuscarActionPerformed
+
+    private void jBModificarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBModificarActionPerformed
+        int idPelicula = ((Pelicula)jCBPeliculas.getSelectedItem()).getIdPelicula();
+        if(idPelicula > 0){
+            int id = idPelicula;
+            String titulo = jTTituloPelicula.getText();
+            
+            Pelicula pelicula = new Pelicula(titulo);
+            pelicula.setIdPelicula(id);
+            peliculaData.modificarPelicula(pelicula);
+            
+            Color colorExito = new Color(7, 110, 46);
+            jLAlerta.setText("Película "+jTTituloPelicula.getText()+ " modificada correctamente");
+            jLAlerta.setForeground(colorExito);
+            cargarDesplegablePeliculas();
+        }
+    }//GEN-LAST:event_jBModificarActionPerformed
+
+    private void jBVaciarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBVaciarActionPerformed
+        limpiar();
+    }//GEN-LAST:event_jBVaciarActionPerformed
+
+    private void jBEliminarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBEliminarActionPerformed
+        int id = ((Pelicula)jCBPeliculas.getSelectedItem()).getIdPelicula();
+        Color colorExito = new Color(7, 110, 46);
+        jLAlerta.setText("Pelicula "+jTTituloPelicula.getText()+" eliminada correctamente");
+        jLAlerta.setForeground(colorExito);
+        peliculaData.bajaPelicula(id);
+        limpiar();
+        cargarDesplegablePeliculas();
+    }//GEN-LAST:event_jBEliminarActionPerformed
+
+    private void limpiar(){
+        jTTituloPelicula.setText("");
+        jTIDPelicula.setText("");
+        jBEliminar.setEnabled(false);
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jBBuscar;
+    private javax.swing.JButton jBEliminar;
+    private javax.swing.JButton jBModificar;
+    private javax.swing.JButton jBVaciar;
+    private javax.swing.JComboBox<Pelicula> jCBPeliculas;
+    private javax.swing.JLabel jLAlerta;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JSeparator jSeparator2;
+    private javax.swing.JTextField jTIDPelicula;
+    private javax.swing.JTextField jTTituloPelicula;
     // End of variables declaration//GEN-END:variables
 }
